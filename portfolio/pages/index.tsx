5d8639eb54f6dd9fc9bdbef75deef0a9628b89ae
Wootae Jeon <jeonwt7@gmail.com>
2022-09-25 22:46:08 -0700

    initial setup

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Wootae Jeon</title>
+        <meta name="description" content="Web portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}></main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+};
+
+export default Home;
